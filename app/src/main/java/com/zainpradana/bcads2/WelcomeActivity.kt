ed48532a6f0898c4b51a9b4c32d1be4f13ed3800
nurzainpradana <pradananurzain@gmail.com>
2020-10-04 03:32:08 +0700

    add motion layout effect

--- app/src/main/java/com/zainpradana/bcads2/WelcomeActivity.kt
+++ app/src/main/java/com/zainpradana/bcads2/WelcomeActivity.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.activity_welcome.*
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_welcome_start)
 
         btn_get_started.setOnClickListener {
             finish()
